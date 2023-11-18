855827baeaef34e00f4bcbbb2d600bf88d0688cb
Adam Cegiełka <adamcegielka@tlen.pl>
2023-11-18 21:09:22 +0100

    tests: add new tests for vailable and pending status

--- tests/api/pet-GET.spec.ts
+++ tests/api/pet-GET.spec.ts
@@ -1,9 +1,35 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('GET pets', () => {
+test.describe('GET/pet/findByStatus Fine Pets by status', () => {
   const baseURL: string = 'https://petstore.swagger.io/v2/';
 
-  test('retrieve pets', async ({ request }) => {
+  test('return successful operation for available', async ({ request }) => {
+    const endpoint: string = 'pet/';
+    const findByStatus: string = 'findByStatus?status=available';
+    const statusCode = 200;
+
+    const response = await request.get(`${baseURL + endpoint + findByStatus}`);
+
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(statusCode);
+
+    console.log(await response.json());
+  });
+
+  test('return successful operation for pending', async ({ request }) => {
+    const endpoint: string = 'pet/';
+    const findByStatus: string = 'findByStatus?status=pending';
+    const statusCode = 200;
+
+    const response = await request.get(`${baseURL + endpoint + findByStatus}`);
+
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(statusCode);
+
+    console.log(await response.json());
+  });
+
+  test('return successful operation for sold', async ({ request }) => {
     const endpoint: string = 'pet/';
     const findByStatus: string = 'findByStatus?status=sold';
     const statusCode = 200;
@@ -12,6 +38,7 @@ test.describe('GET pets', () => {
 
     expect(response.ok()).toBeTruthy();
     expect(response.status()).toBe(statusCode);
+
     console.log(await response.json());
   });
 });
